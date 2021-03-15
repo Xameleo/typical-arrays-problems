abc437af862c7b7e34b887f74c2be707acd628eb
Xameleo <61204795+Xameleo@users.noreply.github.com>
2021-03-16 00:00:49 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,26 @@
+
+exports.min = function min (array) {
+   
+  if (!array  || array.length == 0 )
+    return 0;
+
+    return array.sort((a,b) => b - a).pop();
+  
+}
+
+exports.max = function max (array) {
+  
+  if ( !array  || array.length == 0 )
+  return 0;
+
+    return array.sort((a,b) => a - b).pop();
+  
+}
+
+exports.avg = function avg (array) {
+  if (!array  || array.length == 0 )
+  return 0;
+
+   return array.reduce((a, f) => a + f, 0) / array.length;
+  
+}
